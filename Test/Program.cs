2a13db9a7d6600d7da6aa8bc5238f296f95e6bc2
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2026-08-23 03:42:04 +0700

    Update Test
    Use .NET Framework 4.7.2 for testing the lowest version of .NET to be supported

--- Test/Program.cs
+++ Test/Program.cs
@@ -13,6 +13,7 @@ public class Program
     private static string TestPathInput       = @"G:\Hi3SEA";
     private static string TestPathOutput      = @"G:\Hi3CN-dotnet";
     private static string TestPathPatch       = @"G:\Hi3SEAtoCNExecOnly.lzma2.diff";
+    private static string TestPathPatchLzma   = @"G:\Hi3SEAtoCNExecOnly.lzma.diff";
     private static string TestPathPatchNoComp = @"G:\Hi3SEAtoCNExecOnly.nocomp.diff";
     public static async Task Main()
     {
@@ -37,7 +38,7 @@ public class Program
 
     public static void WriteProgress(long totalProcessed, long totalSize, int written)
     {
-        Console.Write($"{totalProcessed} / {totalSize} {(int)(totalProcessed / (double)totalSize * 100)}%\r");
+        // Console.Write($"{totalProcessed} / {totalSize} {(int)(totalProcessed / (double)totalSize * 100)}%\r");
     }
 
     public static Stream CreateStream()
@@ -48,14 +49,14 @@ public class Program
 
     public static (Stream, bool) CreateStream(long position)
     {
-        FileStream stream = File.Open(TestPathPatch, FileMode.Open, FileAccess.Read, FileShare.Read);
+        FileStream stream = File.Open(TestPathPatchLzma, FileMode.Open, FileAccess.Read, FileShare.Read);
         stream.Position = position;
         return (stream, false);
     }
 
     public static ValueTask<(Stream, bool)> CreateStreamAsync(long position, CancellationToken token)
     {
-        FileStream stream = File.Open(TestPathPatch, FileMode.Open, FileAccess.Read, FileShare.Read);
+        FileStream stream = File.Open(TestPathPatchLzma, FileMode.Open, FileAccess.Read, FileShare.Read);
         stream.Position = position;
         return new ValueTask<(Stream, bool)>((stream, false));
     }
