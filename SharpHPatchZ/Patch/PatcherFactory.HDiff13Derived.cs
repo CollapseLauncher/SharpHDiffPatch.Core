4917b774dba386e1d4922e774146ce35b09149ec
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2026-08-24 04:57:16 +0700

    Allow prefetched stream on any compressed streams

--- SharpHPatchZ/Patch/PatcherFactory.HDiff13Derived.cs
+++ SharpHPatchZ/Patch/PatcherFactory.HDiff13Derived.cs
@@ -114,7 +114,7 @@ internal static partial class PatcherFactory
                                                         Stream[]         streams,
                                                         ChunkSizeInfo[]  sizes)
         {
-            if (compType is not (HDiffCompression.Lzma or HDiffCompression.Lzma2))
+            if (compType is HDiffCompression.Uncompressed)
             {
                 return;
             }
