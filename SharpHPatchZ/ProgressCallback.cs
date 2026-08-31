77a856452e48fd43de9289df4a827bd42fd18fb7
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2026-08-31 12:51:09 +0700

    Add docs to ProgressCallback

--- SharpHPatchZ/ProgressCallback.cs
+++ SharpHPatchZ/ProgressCallback.cs
@@ -5,8 +5,17 @@ using System.Runtime.InteropServices;
 
 namespace SharpHPatchZ;
 
+/// <summary>
+/// A callback delegate for the progress of the patching.
+/// </summary>
+/// <param name="totalProcessed">Determines how many bytes already processed.</param>
+/// <param name="totalSize">Determines how many bytes to be processed in total.</param>
+/// <param name="written">How many bytes is currently being written into the disk.</param>
 public delegate void ProcessedBytesManagedCallback(long totalProcessed, long totalSize, int written);
 
+/// <summary>
+/// Sets progress callback for the patching process.
+/// </summary>
 [StructLayout(LayoutKind.Sequential)]
 public readonly
 #if NET6_0_OR_GREATER
@@ -14,16 +23,29 @@ public readonly
 #endif
     struct ProgressCallback
 {
+    /// <summary>
+    /// The delegate callback for the progress method.
+    /// </summary>
 #if NET6_0_OR_GREATER
-    public readonly delegate* unmanaged[Cdecl]<long, long, int, void> ProcessedBytesCallback;
+    internal readonly delegate* unmanaged[Cdecl]<long, long, int, void> ProcessedBytesCallback;
 #else
-    public readonly ProcessedBytesManagedCallback ProcessedBytesCallback;
+    internal readonly ProcessedBytesManagedCallback ProcessedBytesCallback;
 #endif
 
+    /// <summary>
+    /// Whether the callback pointer is allocated or not.
+    /// </summary>
     internal bool IsAllocated => ProcessedBytesCallback != null;
 
+    /// <summary>
+    /// Creates a new default <see cref="ProgressCallback"/> struct with a NOP callback.
+    /// </summary>
     public ProgressCallback() : this(null) { }
 
+    /// <summary>
+    /// Creates a new <see cref="ProgressCallback"/> with specified callback method.
+    /// </summary>
+    /// <param name="callback"></param>
 #if NET6_0_OR_GREATER
     public ProgressCallback(ProcessedBytesManagedCallback? callback)
     {
@@ -49,11 +71,21 @@ public readonly
     }
 #endif
 
+    /// <summary>
+    /// This method is expected to be exists as a NOP method as a placeholder.
+    /// The <see cref="ProcessedBytesCallback"/> field on .NET 6 uses an unmanaged delegate pointer to the method since it cannot be null.
+    /// So the field is pointing to this method if no callback delegate is set.
+    /// </summary>
 #if NET6_0_OR_GREATER
     [UnmanagedCallersOnly(CallConvs = [typeof(CallConvCdecl)])]
     [SkipLocalsInit]
 #endif
     private static void NopProcessedBytesCallback(long totalProcessed, long totalSize, int written) { }
 
+    /// <summary>
+    /// Creates a new <see cref="ProgressCallback"/> with specified callback method.
+    /// </summary>
+    /// <param name="callback">A method used for the callback of the progress</param>
+    /// <returns>A new <see cref="ProgressCallback"/> struct.</returns>
     public static ProgressCallback CreateFromManaged(ProcessedBytesManagedCallback callback) => new(callback);
 }
